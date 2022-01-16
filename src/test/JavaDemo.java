cf39195243238b816a5cd41a92fb5160fb53f4db
Rishabh <rishabhguptackt@gmail.com>
2022-01-16 05:16:40 +0000

    hello_word

---
+++ src/test/JavaDemo.java
@@ -0,0 +1,13 @@
+package test;
+
+public class JavaDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world");
+		System.out.println("User Name");
+	}
+	
+
+}
